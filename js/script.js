83ea4bbd4c36b98ea626b5c9b4591b2c72aff6ee
mohamed <mohamed@gmail.com>
2025-01-09 16:35:15 +0100

    fix responsive menu for activité and destination

---
+++ js/script.js
@@ -0,0 +1,21 @@
+
+        
+const menuitems=document.getElementById("menuitemes");
+
+menuitems.style.maxHeight="0px";
+
+function showmenu() {
+
+    if( menuitems.style.maxHeight=="0px"){
+        menuitems.style.maxHeight="300px";
+
+    }
+    else{
+        menuitems.style.maxHeight="0px";
+
+    }
+}
+
+
+    
+
